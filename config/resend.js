c88144f65612d2c1dfabefdfdef3701e301b0381
Jenivaa M <m.jenivaakoilpillai2006@gmail.com>
2026-05-20 19:38:37 +0530

    Fix resend

--- config/resend.js
+++ config/resend.js
@@ -1,10 +1,8 @@
-// config/resend.js
-
 const FROM_EMAIL = 'onboarding@resend.dev';
 const FROM_NAME = 'Paddox F1';
 
 const sendEmail = async () => {
-  console.log('Email sending disabled');
+  console.log('Email temporarily disabled');
   return { success: true };
 };
 
