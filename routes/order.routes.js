d2a5dd7b1abcf7974e68c38f17a5dc8f4b8748a3
Jenivaa M <m.jenivaakoilpillai2006@gmail.com>
2026-05-24 22:33:54 +0530

    Connect realtime admin orders

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -10,10 +10,11 @@ const { protect, adminOnly } = require('../middleware/auth.middleware');
 router.use(protect);
 router.post('/',               order.placeOrder);
 router.get('/',                order.getMyOrders);
-router.get('/:id',             order.getOrder);
-router.get('/:id/track',       order.trackOrder);
-router.put('/:id/cancel',      order.cancelOrder);
-router.get('/admin/all',       adminOnly, order.getAllOrders);
-router.put('/admin/:id/status',adminOnly, order.updateOrderStatus);
+router.get('/admin/all',        adminOnly, order.getAllOrders);
+router.put('/admin/:id/status', adminOnly, order.updateOrderStatus);
+
+router.get('/:id',        order.getOrder);
+router.get('/:id/track',  order.trackOrder);
+router.put('/:id/cancel', order.cancelOrder);
 
 module.exports = router;
