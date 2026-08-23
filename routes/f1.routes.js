a36c592973a494df08a1850c23264fc01ffb7e9d
Jenivaa M <m.jenivaakoilpillai2006@gmail.com>
2026-08-23 20:31:06 +0530

    feat(pitwall): expose FastF1 replay API

--- routes/f1.routes.js
+++ routes/f1.routes.js
@@ -6,6 +6,7 @@ const router  = express.Router();
 const f1      = require('../controllers/f1.controller');
 const f1Career = require('../controllers/f1Career.controller');
 const racePrediction = require('../controllers/racePrediction.controller');
+const pitwallReplay = require('../controllers/pitwallReplay.controller');
 const { protect, adminOnly } = require('../middleware/auth.middleware');
 const { f1Limiter } = require('../middleware/rateLimit.middleware');
 
@@ -23,6 +24,8 @@ router.get('/sessions',               f1.getSessions);
 router.get('/live',                   f1.getLiveSession);
 router.get('/pitwall/weekend',        f1.getPitWallWeekend);
 router.get('/pitwall/session',        f1.getPitWallSession);
+router.get('/pitwall/replay/manifest', pitwallReplay.getManifest);
+router.get('/pitwall/replay/frame',    pitwallReplay.getFrame);
 router.post('/pitwall/predict',       racePrediction.predictPitWallSession);
 router.post('/cache/clear',           protect, adminOnly, f1.clearCache);
 
